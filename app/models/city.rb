e3c3388c2a39ab511b3fc0f9b71c0bb93e4edc6c
weixing1985 <weixing1985@gmail.com>
2014-08-18 18:16:57 -0700

    xw - fix autocomplete issue

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,7 @@
 class City < ActiveRecord::Base
 
   belongs_to :country
+  default_scope { order(contributors: :desc) }
   scope :searchables, lambda {where("searchable is true")}
   def self.list_of_city_and_country_full_names_cn
     full_named_cities = self.searchables.all.inject([]){|result, element| result << (element[:name_cn].force_encoding("utf-8")+", "+element.country[:name_cn].force_encoding("utf-8"))}
